0a4395b1d8d2c8e3f92527e9d67c47a4d0f09468
ResonanceForge <friedrichswun@gmail.com>
2025-08-06 09:39:03 +0800

    Off-by-one error

--- short-id-filter.py
+++ short-id-filter.py
@@ -8,7 +8,7 @@ with open("list.meta.yml", 'r') as file:
         if re.match(r'^\s*short-id', line):
            if not re.match(r'^\s*short-id: 7a2b', line):
                lines[line_number] = '    short-id: 6a2b'
-               print(f"編輯第{line_number}行的節點short-id，從{line}到short-id: 7a2b")
+               print(f"編輯第{line_number+1}行的節點short-id，從{line}到short-id: 7a2b")
     content_new = "\n".join(lines)
     file.close()
 
